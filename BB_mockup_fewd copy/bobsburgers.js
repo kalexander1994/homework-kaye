af043d50ddb1d09064b9645607e4f5e2b32dadeb
K.Alexander <kalexander1994@hotmail.com>
2018-09-26 12:47:50 -0700

    added project

---
+++ BB_mockup_fewd copy/bobsburgers.js
@@ -0,0 +1,34 @@
+
+//ENTIRE SITE
+//make nav bar responsive
+//make entire site responsive--resizes appropriately, no matter what the browser/window size
+//loading displays Bobs Burgers 'burger drop opening sequence' animation
+
+//LANDING PAGE PSEUDOCODE
+//DISCLAIMER
+	//APPEARS when page is loaded, becomes an alert, and can be clicked away
+	//remains sticky after user clicks the X to minimize or exit disclaimer
+//HEADER
+	//when HOVERED
+		//Image darkens and the "Burger of the Day" text appears
+	//when CLICKED
+		//Text becomes white and underlined to indicate text has been clicked.
+	//when page is REFRESHED
+		//image and text changes. These are both picked at random between 10 images and 5 texts.
+		//these particular images hyperlink to their respective "Burger of the Day" location
+//Individial icons
+	//when HOVER
+		//Image darkens and the name of the dish becomes white and underlined.
+	//when CLICKED
+		//a pop up window displays the name, price, a brief quote, and brief description.
+
+//individual containers
+	//when HOVER
+		//see previous
+	//when CLICKED
+		//see previous
+
+
+//BURGER OF THE DAY PSEUDOCODE
+		//for nav bar instructions, see above
+		//same above for images, containers, etc.
